81f6ea58ff40a0fb7a17dfa7d094c594edda0e7d
Harysson <haryssonsoares@gmail.com>
2024-10-13 22:09:11 -0300

    fix: handle error if any brand was found

--- src/logo/logo.service.ts
+++ src/logo/logo.service.ts
@@ -5,26 +5,32 @@ import { getBrandByDomain, getBrandByName } from 'src/service/googleKnowledge';
 @Injectable()
 export class LogoService {
   async getLogo({ name }: { name: string }) {
-    const reference = `logo-${name}`;
-    if (!logoInMemory.hasItem(reference)) {
-      const brands = await getBrandByName({ name });
+    try {
+      const reference = `logo-${name}`;
+      if (!logoInMemory.hasItem(reference)) {
+        const brands = await getBrandByName({ name });
 
-      const domain = brands.data.find(
-        (brand) => brand.name.toLowerCase() === name.toLowerCase(),
-      )?.domain;
+        const domain = brands.data.find(
+          (brand) =>
+            brand.name.toLowerCase() === name.toLowerCase() ||
+            brand.name.includes(name),
+        )?.domain;
 
-      const logo = await getBrandByDomain({ domain });
+        const logo = await getBrandByDomain({ domain });
 
-      const url = logo.data.logos
-        .find((lg) => lg.theme === 'light')
-        ?.formats.find((frmt) => frmt.format === 'svg')?.src;
+        const url = logo.data.logos
+          .find((lg) => lg.theme === 'light')
+          ?.formats.find((frmt) => frmt.format === 'svg')?.src;
 
-      logoInMemory.storeExpiringItem(
-        reference,
-        url,
-        process.env.NODE_ENV === 'test' ? 5 : 3600 * 24, // if test env expire in 5 miliseconds else 1 day
-      );
+        logoInMemory.storeExpiringItem(
+          reference,
+          url,
+          process.env.NODE_ENV === 'test' ? 5 : 3600 * 24, // if test env expire in 5 miliseconds else 1 day
+        );
+      }
+      return logoInMemory.retrieveItemValue(reference);
+    } catch (error) {
+      return null;
     }
-    return logoInMemory.retrieveItemValue(reference);
   }
 }
