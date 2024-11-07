196ca1fa6dd3f9d1ca07ec49541cce16575559c7
Harysson <haryssonsoares@gmail.com>
2024-11-07 18:51:26 -0300

    feat: add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,6 +16,17 @@ async function bootstrap() {
       String(process.env.SECURITY_URL),
     ],
   });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(Number(process.env.PORT) || 3000, () => {
     console.info('RUNNING IN PORT = ', Number(process.env.PORT) || 3000);
   });
